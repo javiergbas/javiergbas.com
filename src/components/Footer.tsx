49e58eaae2fed72e4af3484185cf0e3153582a4c
javiergbas <jgbas67@gmail.com>
2026-04-13 10:50:59 +0200

    Add social links to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,28 @@
-import MadridPopup from './MadridPopup.tsx'
+import MadridPopup from "./MadridPopup.tsx";
 
 const Footer = () => (
-    <footer className="max-w-3xl mx-auto px-8 md:px-6 pb-8 text-sm text-gray-600 text-center">
-    Crafted with ❤️ and 🤖 in sunny <MadridPopup /> · © {new Date().getFullYear()}
+  <footer className="max-w-3xl mx-auto px-8 md:px-6 pb-8 text-sm text-gray-600 text-center">
+    Crafted with ❤️ and 🤖 in sunny <MadridPopup />
+    {" · "}
+    <a
+      href="https://www.linkedin.com/in/javiergutierrezbas/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="underline"
+    >
+      LinkedIn
+    </a>
+    {" · "}
+    <a
+      href="https://github.com/javiergbas"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="underline"
+    >
+      Github
+    </a>
+    {" · "}© {new Date().getFullYear()}
   </footer>
-    )
-  
-  export default Footer
+);
+
+export default Footer;
